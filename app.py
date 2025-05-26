02583a24ce2455dc6c7b056899e97708a762065a
codingchqh <dlgustj723@gmail.com>
2025-05-26 17:11:37 +0900

    1

--- app.py
+++ app.py
@@ -192,17 +192,18 @@ if current_page_value == "main":
     
     # 메인 페이지 헤더에 군청색 배경 박스 추가
     st.markdown("""
-        <div style="
-            background-color: black; /* 검정색 */
-            padding: 10px; /* 내부 여백 */
-            margin-bottom: 15px; /* 아래쪽 간격 */
-            text-align: center; /* 텍스트 중앙 정렬 */
-        ">
-            <h2></h2> <!-- Streamlit 헤더 대신 h2 사용 -->
-        </div>
+    <div style="
+        background-color: black;
+        padding: 10px;
+        margin-bottom: 15px;
+        text-align: center;
+    ">
+        <img src="xndx8.png" alt="일반 이미지" style="max-width: 100%; height: auto;"/>
+    </div>
     """, unsafe_allow_html=True)
-    
+
     st.write("이곳은 메인 페이지입니다.")
+
 
 elif current_page_value == "page1":
     # 페이지 1 헤더에 검정색 배경 박스 추가
