3ada38418ccda71bad011b8b82837e526859f122
codingchqh <dlgustj723@gmail.com>
2025-05-26 17:35:46 +0900

    3

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def set_png_as_bg(png_file):
     }}
     </style>
     """
-    img_base64 = get_base64_of_bin_file("xndx8.png")
+    img_base64 = get_base64_of_bin_file("xndx7.png")
 
     html_img = f"""
     <div style="
@@ -203,7 +203,7 @@ if current_page_value == "main":
 
     
     # 메인 페이지 헤더에 군청색 배경 박스 추가
-    img_base64 = get_base64_of_bin_file("xndx8.png")
+    img_base64 = get_base64_of_bin_file("xndx7.png")
     html_img = f"""
     <div style="
         background-color: black;
