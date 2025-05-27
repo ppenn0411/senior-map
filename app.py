e3699ab120c385e659855ffc6dc9ddfc2eab3115
codingchqh <dlgustj723@gmail.com>
2025-05-27 17:03:50 +0900

    23

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ st.markdown(
     <style>
         /* Streamlit 제목 글자 크기 및 위치 조절 */
         .stApp h1 {
-            font-size: 3em !important; /* 제목 글자 크기 설정 (원하는 크기로 조절 가능) */
+            font-size: 5em !important; /* 제목 글자 크기 설정 (원하는 크기로 조절 가능) */
             margin-top: -30px !important; /* 제목을 위로 약간 올립니다 (값 조절 가능) */
             /* white-space: nowrap; */ /* 줄바꿈 방지 */
             /* overflow: hidden; */ /* 넘치는 텍스트 숨김 */
