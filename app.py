8409497f6f4ddf86d9e271d077f007dbd52f1572
codingchqh <dlgustj723@gmail.com>
2025-05-27 12:11:53 +0900

    123

--- app.py
+++ app.py
@@ -207,9 +207,7 @@ if current_page_value == "main":
         margin-bottom: 15px;
         text-align: center;
     ">
-        <body style="margin: 0;">
-  <img src="{image_base64}" style="width: 100vw; height: 100vh; object-fit: cover;"/>
-</body>
+        <img src="{image_base64}" style="width: 100vw; height: 100vh; object-fit: contain;"/>
 
     </div>
     """, unsafe_allow_html=True)
